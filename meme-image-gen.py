cfb217bf9514defe45ab0e06645fe1c95fd6fe2e
grant0417 <grantgurvis@gmail.com>
2019-11-16 11:50:27 -0500

    added meme text

--- meme-image-gen.py
+++ meme-image-gen.py
@@ -1,6 +1,10 @@
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageFont, ImageDraw
 import os, random
 
+draw = ImageDraw
+
 img = Image.open("meme_img/" + random.choice(os.listdir("meme_img")))
+
+font = ImageFont.load("")
 
 print(img)
